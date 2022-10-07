f533bc09c92818a8db1288005a624ac601228f54
Chris <chrisgordon101@gmail.com>
2022-10-07 17:36:43 +1100

    basic XMLHTTP request added

--- static/loadS3.js
+++ static/loadS3.js
@@ -1,16 +1,6 @@
 function listFiles() {
-    AWS.config.update({
-        region: '',
-        accessKeyId: '',
-        secretAccessKey: ''
-    });
-
-    var s3 = new AWS.S3();
-
-    var params = { Bucket: '', Key: 'test.txt' };
-
-    s3.getObject(params, function(err, data){
-        if (err) console.log(err, err.stack); // an error occurred
-        else     console.log(data); 
-    });
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", "");
+    req.send();
 }
