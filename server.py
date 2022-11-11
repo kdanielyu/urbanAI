2ea4837b479de9017f4c000ea4abea490a20a150
Chris <chrisgordon101@gmail.com>
2022-11-11 13:04:30 +1100

    Redirect base directory to index.html

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import os.path
 import boto3
 
-from flask import Flask, Response, send_file
+from flask import Flask, Response, send_file, redirect
 
 
 app = Flask(__name__, static_url_path='/')
 
 @app.route('/', methods=['GET'])
 def index():
-    return Response(status=404)
+    return redirect("/index.html", code=302)
 
 @app.route('/get-object', methods=['GET'])
 def get_object_AWS():
